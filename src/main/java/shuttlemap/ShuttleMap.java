152241c8eb023b1247f5f64a6478bacafb3706ff
valoeghese <valoeghesemods@gmail.com>
2020-08-25 14:58:15 +1200

    move old code to another class

--- src/main/java/shuttlemap/ShuttleMap.java
+++ src/main/java/shuttlemap/ShuttleMap.java
@@ -18,10 +18,7 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
-import org.cadixdev.bombe.type.signature.FieldSignature;
-import org.cadixdev.bombe.type.signature.MethodSignature;
 import org.cadixdev.lorenz.MappingSet;
-import org.cadixdev.lorenz.io.proguard.ProGuardFormat;
 import org.cadixdev.lorenz.io.proguard.ProGuardReader;
 import org.cadixdev.lorenz.model.ClassMapping;
 import org.cadixdev.lorenz.model.FieldMapping;
@@ -30,7 +27,6 @@ import org.cadixdev.lorenz.model.MethodMapping;
 
 import net.fabricmc.lorenztiny.TinyMappingFormat;
 import net.fabricmc.mapping.reader.v2.MappingParseException;
-import tk.valoeghese.motjin.map.parser.ObfuscationMap;
 
 public class ShuttleMap {
 	public static void main(String[] args) throws MappingParseException, IOException {
@@ -54,16 +50,6 @@ public class ShuttleMap {
 				"official", "intermediary");
 
 		writeTinyLorenz("shuttle.tiny", o2i, o2m);
-	}
-
-	public static void oldMain() throws MappingParseException, IOException {
-		MappingSet mojang = new ProGuardFormat().createReader(
-				Files.newInputStream(Paths.get("./client.txt")))
-				.read().reverse(); // official -> mojang
-
-		ObfuscationMap intermediary = ObfuscationMap.parseTiny("./mappings.tiny");
-
-		writeTiny("shuttle.tiny", intermediary, mojang);
 	}
 
 	// from motjin
@@ -174,66 +160,6 @@ public class ShuttleMap {
 
 		for (InnerClassMapping map : preMap.getInnerClassMappings()) {
 			ic(map, callback);
-		}
-	}
-
-	// from motjin
-	private static void writeTiny(String file, ObfuscationMap intermediary, MappingSet mojang) {
-		try (PrintWriter writer = new PrintWriter(file, "UTF-8")) {
-			writer.println("v1\tofficial\tintermediary\tnamed");
-
-			intermediary.forEachObf((obf, intermediaryEntry) -> {
-				StringBuilder output = new StringBuilder();
-				System.out.println("Mapping\t" + intermediaryEntry.getMappedName());
-
-				// Add mojang name to intermediary class entry
-				Optional<? extends ClassMapping<?, ?>> mojmapEntry = mojang.computeClassMapping(obf);
-
-				if (mojmapEntry.isPresent()) {
-					intermediaryEntry.setFinalColumnMapping(mojmapEntry.get().getDeobfuscatedName());
-				}
-
-				// Add to output tiny
-				output.append(intermediaryEntry.toString());
-
-				if (mojmapEntry.isPresent()) {
-					ClassMapping<?, ?> mojmapEntry_ = mojmapEntry.get();
-
-					// Process Fields
-					intermediaryEntry.fields.forEach(intermediaryFieldEntry -> {
-						// Get field entry and set final column mapping
-						Optional<? extends FieldMapping> mojmapFieldEntry = mojmapEntry_.computeFieldMapping(FieldSignature.of(intermediaryFieldEntry.obfName, intermediaryFieldEntry.descriptor));
-
-						if (mojmapFieldEntry.isPresent()) {
-							intermediaryFieldEntry.setFinalColumnMapping(mojmapFieldEntry.get().getDeobfuscatedName());
-						}
-
-						// Add to output
-						output.append("\n").append(intermediaryFieldEntry.toString());
-					});
-
-					// Process Methods
-					intermediaryEntry.methods.forEach(intermediaryMethodEntry -> {
-						// Get method entry and set final column mapping
-						Optional<? extends MethodMapping> mojmapMethodEntry = mojmapEntry_.getMethodMapping(MethodSignature.of(intermediaryMethodEntry.obfName, intermediaryMethodEntry.signature));
-
-						if (mojmapMethodEntry.isPresent()) {
-							if (intermediaryMethodEntry.getMappedName().startsWith("method")) {
-								intermediaryMethodEntry.setFinalColumnMapping("mc_" + mojmapMethodEntry.get().getDeobfuscatedName());
-							} else {
-								intermediaryMethodEntry.setFinalColumnMapping(mojmapMethodEntry.get().getDeobfuscatedName());
-							}
-						}
-
-						// Add to output
-						output.append("\n").append(intermediaryMethodEntry.toString());
-					});
-
-					writer.println(output.toString());
-				}
-			});
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			throw new UncheckedIOException(e);
 		}
 	}
 }
